7b709fbb80278648f67e756d81fa17893115c5dc
lukad <luka.d.stojiljkovic@gmail.com>
2023-06-22 20:59:09 +0200

    UI layout, fixed BtZero width

--- app/src/main/java/com/lukastojiljkovic/calculator_app/MainActivity.kt
+++ app/src/main/java/com/lukastojiljkovic/calculator_app/MainActivity.kt
@@ -2,10 +2,17 @@ package com.lukastojiljkovic.calculator_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    var i:  Int = 0
+    fun onDigit(view: View) {
+
     }
 }
